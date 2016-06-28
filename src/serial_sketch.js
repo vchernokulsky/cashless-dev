245803bc4b69ab1316376c58d569aa380d51c866
sauron <vladimir.chernokulsky@gmail.com>
2016-06-28 18:20:23 +0300

    small changes

--- src/serial_sketch.js
+++ src/serial_sketch.js
@@ -1,6 +1,8 @@
 Serial4.setup(9600);
-
 Serial4.on('data', function (data) {
-  console.log("<MSG>: " + data);
+  console.log("<MSG>: " + data.charCodeAt(0).toString(16));
   Serial4.write([0]);
 });
+
+console.log("CPP unit started");
+
