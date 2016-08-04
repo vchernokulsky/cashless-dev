29520e379f4ef80d7343220104b0f13f83de20e3
sauron <vladimir.chernokulsky@gmail.com>
2016-08-04 12:34:04 +0300

    extend log info

--- src/mic/MDBAdapter/cashless_protocol.c
+++ src/mic/MDBAdapter/cashless_protocol.c
@@ -63,7 +63,7 @@ void process_inactive(unsigned char* data) {
 			fill_mbd_command(&resp, resp_ack, 1);
 			send_mdb_command(&resp);
 			fill_mbd_command(&delay_cmd, resp_just_reset, 2);
-			log("(INACTIVE)|RECV:RESET ; SEND: ACK\n");
+			log("(INACTIVE)|RECV:RESET; SEND: ACK; DELAY: JUST RESET\n");
 		break;
 		case POLL:
 			if(delay_cmd.length>0) {
@@ -90,7 +90,7 @@ void process_inactive(unsigned char* data) {
 					fill_mbd_command(&delay_cmd, resp_config_data, 9);
 					fill_mbd_command(&resp, resp_ack, 1);
 					send_mdb_command(&resp);
-					log("(INACTIVE)|RECV:SETUP [config data] ; SEND: ACK\n");
+					log("(INACTIVE)|RECV:SETUP [config data] ; SEND: ACK; DELAY: CONFIG DATA\n");
 				break;
 				case 0x01: // Max / Min Price
 					fill_mbd_command(&resp, resp_ack, 1);
@@ -107,17 +107,18 @@ void process_inactive(unsigned char* data) {
 					send_mdb_command(&resp);
 					fill_mbd_command(&delay_cmd, resp_request_id, 31);
 					_delay_state = ST_DISABLED;
-					log("(INACTIVE)|RECV:EXPANSION [request id]; SEND: ACK\n");
+					log("(INACTIVE)|RECV:EXPANSION [request id]; SEND: ACK; DELAY: DEV ID\n");
 				break;
 			}
 		break;
 		default:
-			log("UNKNOWN COMMAND");
+			log("(INACTIVE)|UNKNOWN COMMAND\n");
 	}
 }
 
 void process_disabled(unsigned char* data){
 	char resp_ack[1] = {0x00};
+	char resp_just_reset[2] = {0x00, 0x00};
 	char resp_cmd_out_of_sequence[2] = {0x0B, 0x0B};
 	char pesn_expansion_id_request[4] = {0x01, 0x02, 0xFF, 0x02};
 
@@ -129,14 +130,16 @@ void process_disabled(unsigned char* data){
 			fill_mbd_command(&resp, resp_ack, 1);
 			send_mdb_command(&resp);
 			_cashless_state = ST_INACTIVE;
-			log("(DISABLED)|RECV:RESET ; SEND: ACK\n");
+			// make delay_cmd: resp_just_reset
+			fill_mbd_command(&delay_cmd, resp_just_reset, 2);
+			log("(DISABLED)|RECV:RESET; SEND: ACK; DELAY: JUST RESET\n");
 		break;
 		case POLL:
 			// process RESET command
 			if (delay_cmd.length>0){
 				send_mdb_command(&delay_cmd);
 				clear_mdb_command(&delay_cmd);
-				log("(DISABLED)|RECV:POLL ; SEND: delay_cmd\n");
+				log("(DISABLED)|RECV:POLL; SEND: delay_cmd\n");
 			} else {
 				fill_mbd_command(&resp, resp_ack, 1);
 				send_mdb_command(&resp);
@@ -157,7 +160,7 @@ void process_disabled(unsigned char* data){
 				case 0x00: // Reader Disable
 					fill_mbd_command(&resp, resp_ack, 1);
 					send_mdb_command(&resp);
-					log("(DISABLED)|RECV:READER [reader disable]; SEND: Reader Disable\n");
+					log("(DISABLED)|RECV:READER [reader disable]; SEND: ACK\n");
 				break;
 				case 0x02: // Reader Cancel
 					fill_mbd_command(&delay_cmd,resp_cmd_out_of_sequence,2);
@@ -173,18 +176,27 @@ void process_disabled(unsigned char* data){
 			}
 		break;
 		default:
-			log("UNKNOWN COMMAND");
+			log("(DISABLED)|UNKNOWN COMMAND\n");
 	}
 }
 
 void process_enabled(unsigned char* data){
 	char resp_ack[1] = {0x00};
+	char resp_just_reset[2] = {0x00, 0x00};
 	char resp_display[34] = {0x02, 100, 0x2E,0x36,0x39,0x3D,0x42,0x14,0x3C,0x35,0x38,0x3D,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14,0x14};
 	char result[4] = {0,0,0,0};
 
 	int cmdId =    (int)(data[0] & MASK_CMD);
 	int subCmdId = 0xFF;
 	switch (cmdId){
+		case RESET:
+			fill_mbd_command(&resp, resp_ack, 1);
+			send_mdb_command(&resp);
+			fill_mbd_command(&delay_cmd, resp_just_reset, 2);
+			// change state to INACTIVE
+			_cashless_state = ST_INACTIVE;
+			log("(ENABLED)|RECV:RESET ; SEND: ACK\n");
+			break;
 		case POLL:
 			cur_balance = read_balance();
 			if (cur_balance > 0) {  // BEGIN SESSION
@@ -225,12 +237,13 @@ void process_enabled(unsigned char* data){
 			}
 			break;
 		default:
-			log("(ENABLED)|UNKNOWN COMMAND");
+			log("(ENABLED)|UNKNOWN COMMAND\n");
 	}
 }
 
 void process_session_idle(unsigned char* data){
 	char resp_ack[1] = {0x00};
+	char resp_just_reset[2] = {0x00, 0x00};
 	char resp_session_cancel[2] = {0x04,0x04};
 	char resp_end_session[2] = {0x07,0x07};
 
@@ -238,6 +251,14 @@ void process_session_idle(unsigned char* data){
 	int cmdId =    (int)(data[0] & MASK_CMD);
 	int subCmdId = -1;
 	switch (cmdId){
+		case RESET:
+			fill_mbd_command(&resp, resp_ack, 1);
+			send_mdb_command(&resp);
+			fill_mbd_command(&delay_cmd, resp_just_reset, 2);
+			// change state to INACTIVE
+			_cashless_state = ST_INACTIVE;
+			log("(IDLE)|RECV:RESET ; SEND: ACK\n");
+			break;
 		case VEND:
 			subCmdId = (int)data[1];
 			switch(subCmdId) {
@@ -259,7 +280,7 @@ void process_session_idle(unsigned char* data){
 					log("(IDLE)|RECV:VEND(Session Complete) ; SEND: ACK\n");
 				break;
 				default:
-					log("UNKNOWN SUBCOMMAND");
+					log("(IDLE)|UNKNOWN SUBCOMMAND\n");
 			}
 		break;
 		case POLL:
@@ -300,18 +321,27 @@ void process_session_idle(unsigned char* data){
 			}
 		break;
 		default:
-			log("UNKNOWN COMMAND");
+			log("(IDLE)|UNKNOWN COMMAND\n");
 	}
 }
 
 void process_vend(unsigned char* data){
 	char resp_ack[1] = {0x00};
+	char resp_just_reset[2] = {0x00, 0x00};
 	char chk;
 	int cmdId =    (int)(data[0] & MASK_CMD);
 	char commad_to_send[4] = {0, 0, 0, 0};
 
 	int subCmdId = -1;
 	switch (cmdId){
+		case RESET:
+			fill_mbd_command(&resp, resp_ack, 1);
+			send_mdb_command(&resp);
+			fill_mbd_command(&delay_cmd, resp_just_reset, 2);
+			// change state to INACTIVE
+			_cashless_state = ST_INACTIVE;
+			log("(VEND)|RECV:RESET ; SEND: ACK\n");
+			break;
 		case POLL:
 			// No CoinMechanismPushes No SessionFailure
 			// VEND APPROVED
@@ -365,12 +395,12 @@ void process_vend(unsigned char* data){
 					log("(VEND)|RECV:VEND[SESSION COMPLETE]; SEND: ACK\n");
 				break;
 				default:
-					log("UNKNOWN SUBCOMMAND");
+					log("(VEND)|UNKNOWN SUBCOMMAND\n");
 
 			}
 		break;
 		default:
-			log("UNKNOWN COMMAND");
+			log("(VEND)|UNKNOWN COMMAND\n");
 	}
 }
 
