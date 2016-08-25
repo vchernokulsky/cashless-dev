59ca1d734e756b4ddb607b2c9a3c170e6435919e
sauron <vladimir.chernokulsky@gmail.com>
2016-08-25 15:20:38 +0300

    simple changes

--- _PoC/SLSrvTest/Form1.cs
+++ _PoC/SLSrvTest/Form1.cs
@@ -14,12 +14,9 @@ namespace SLSrvTest
 {
     public partial class Form1 : Form
     {
-        private Button button1;
-        private Button button2;
-        // const string urlConst = @"https://172.16.0.68/slsrv"; // по локальной сети
+        const string urlConst = @"http://62.152.90.246:60080/slsrv"; // по локальной сети
 
-        //const string urlConst = @"https://sync.sportlifeclub.ru/slsrv";
-        const string urlConst = @"http://sync.sportlifeclub.ru:60080/slsrv";
+        // const string urlConst = @"http://sync.sportlifeclub.ru/slsrv";
 
         public Form1()
         {
@@ -30,32 +27,39 @@ namespace SLSrvTest
         {
             WebClientEx client = new WebClientEx();
             System.Net.ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(AcceptAllCertifications);
+
             client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
-            string loginParameters = @"UserName=SLSrv78621&Password=SL123ksd___uuxx_hf_____123123SL";
 
-//            string ss = client.UploadString(urlConst + "/account/login", loginParameters);
-//            // по внутренней сети используется самоподписанный сертификат: 
-//            if (ss.ToLower() != "ok")
-//            {
-//                MessageBox.Show("Ошибка авторизации: " + ss );
-//                return;
-//            }
-//            else
-//            {
-//                // Успех
-//            }
+            //string loginParameters = @"UserName=SLSrv78621&Password=SL123ksd___uuxx_hf_____123123SL";
 
-            Thread.Sleep(100);
-            string chipId = "011000000168435012";
+            //string ss = client.UploadString(urlConst + "/Account/DoLogin", loginParameters);
+
+            // по внутренней сети используется самоподписанный сертификат: 
+            /*
+            if (ss.ToLower() != "ok")
+            {
+                MessageBox.Show("Ошибка авторизации: " + ss );
+                return;
+            }
+            else
+            {
+                // Успех
+            }
+
+            Thread.Sleep(100);*/
+
+
+            string chipId = "00112233445566778899AABBCCDDEEFF";
             string stateParameters = @"chip=" + chipId;
 
             client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
             string s2 = client.UploadString(urlConst + "/Chip/GetState", stateParameters);
 
-//            string logoutParameters = "";
-//            client.UploadString(urlConst + "/Account/LogOut", logoutParameters);
+            //string logoutParameters = "";
+            //client.UploadString(urlConst + "/Account/LogOut", logoutParameters);
 
             MessageBox.Show(s2);
+
         }
 
 
@@ -88,12 +92,12 @@ namespace SLSrvTest
             }
 
             Thread.Sleep(100);
-            
 
-            string writeOffParameters = @"chip=011000000168435012&srvid=8633&price=150" ; // услуга 8633, цена 1 руб. 50 коп.
+
+            string writeOffParameters = @"dev=1&chip=00112233445566778899AABBCCDDEEFF&srvid=8633&price=150"; // услуга 8633, цена 1 руб. 50 коп.
 
             client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
-            string s2 = client.UploadString(urlConst + "/Chip/WriteOff", writeOffParameters);
+            string s2 = client.UploadString(urlConst + "/Chip/WriteOffV2", writeOffParameters);
 
             string logoutParameters = "";
             client.UploadString(urlConst + "/Account/LogOut", logoutParameters);
@@ -101,40 +105,78 @@ namespace SLSrvTest
             MessageBox.Show(s2);
         }
 
-        private void InitializeComponent()
+        private void button3_Click(object sender, EventArgs e)
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(13, 43);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // Form1
-            // 
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.ResumeLayout(false);
+            WebClientEx client = new WebClientEx();
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(AcceptAllCertifications);
 
+            client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+
+            string loginParameters = @"UserName=SLSrv78621&Password=SL123ksd___uuxx_hf_____123123SL";
+
+            string ss = client.UploadString(urlConst + "/Account/DoLogin", loginParameters);
+
+            // по внутренней сети используется самоподписанный сертификат: 
+
+            if (ss.ToLower() != "ok")
+            {
+                MessageBox.Show("Ошибка авторизации: " + ss);
+                return;
+            }
+            else
+            {
+                // Успех
+            }
+
+            Thread.Sleep(100);
+
+
+            string writeOffParameters = @"dev=1000&chip=00112233445566778899AABBCCDDEEFF&minutes=15"; // услуга 8633, цена 1 руб. 50 коп.
+
+            client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+            string s2 = client.UploadString(urlConst + "/Chip/WriteOffSolV2", writeOffParameters);
+
+            string logoutParameters = "";
+            client.UploadString(urlConst + "/Account/LogOut", logoutParameters);
+
+            MessageBox.Show(s2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            WebClientEx client = new WebClientEx();
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(AcceptAllCertifications);
+
+            client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+
+            string loginParameters = @"UserName=SLSrv78621&Password=SL123ksd___uuxx_hf_____123123SL";
+
+            string ss = client.UploadString(urlConst + "/Account/DoLogin", loginParameters);
+
+            // по внутренней сети используется самоподписанный сертификат: 
+
+            if (ss.ToLower() != "ok")
+            {
+                MessageBox.Show("Ошибка авторизации: " + ss);
+                return;
+            }
+            else
+            {
+                // Успех
+            }
+
+            Thread.Sleep(100);
+
+
+            string writeOffParameters = @"dev=1000&chip=00112233445566778899AABBCCDDEEFF&writeoffid=1&success=1"; // услуга 8633, цена 1 руб. 50 коп.
+
+            client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+            string s2 = client.UploadString(urlConst + "/Chip/WriteOffCommit", writeOffParameters);
+
+            string logoutParameters = "";
+            client.UploadString(urlConst + "/Account/LogOut", logoutParameters);
+
+            MessageBox.Show(s2);
         }
 
     }
